627885378c0d85e29f190b0c411a11611ce81dc1
Krishna Kumar <106882782+krishna5867@users.noreply.github.com>
2023-01-02 17:23:36 +0530

    fix issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,26 +5,35 @@ import { useSelector } from 'react-redux';
 const Navbar = () => {
     const items = useSelector((state) => state.cart);
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark d-flex">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
             <div className="container-fluid">
-                <div>
-                <Link className="navbar-brand" to="/"><b>Store</b></Link>
-                </div>
-                <div>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <Link className="navbar-brand mx-4" to="/"><b>Store</b></Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+            </div>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <div>
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
-                        <li className="nav-item mx-5">
-                            <Link className="nav-link" to="/cart">Cart ({items.length})</Link>
+                        <li className="nav-item mx-4  position-relative">
+                            <Link className="nav-link" to="/cart"><i className="fas fa-cart-plus"></i>
+                                <span className="position-absolute top-1 start-100 translate-middle badge rounded-pill bg-success">
+                                    {items.length}
+                                    <span className="visually-hidden">unread messages</span>
+                                </span>
+                            </Link>
                         </li>
-                        
                     </ul>
-                </div>
                 </div>
             </div>
         </nav>
--- src/components/Products.js
+++ src/components/Products.js
@@ -6,7 +6,7 @@ import { fetchProducts } from '../store/productSlice';
 import { STATUSES } from '../store/productSlice';
 
 
-const Products = ({product}) => {
+const Products = () => {
     const dispatch = useDispatch();
     const { data: products, status } = useSelector((state) => state.product);
 
@@ -19,23 +19,23 @@ const Products = ({product}) => {
     };
 
     if (status === STATUSES.LOADING) {
-        return <h2>Loading....</h2>;
+        return <h2>Loading Data....</h2>;
     }
 
     if (status === STATUSES.ERROR) {
         return <h2>Something went wrong!</h2>;
     }
     return (
-        <Container fluid className='col-10 w-100'>
+        <Container className='col-10 w-100'>
             <Row>
                     {
                             products.map((product) => (
-                            <CardBody key={product.id} style={{width:'12rem'}} className="my-4">
+                            <CardBody key={product.id} style={{width:'12rem'}} className="my-4 p-3 m-2 shadow">
                                 <img src={product.image} alt="img" style={{ width: '10rem' }} height="200px" />
                                 <div style={{height:'50px'}} className="overflow-hidden mt-2">
                                     {product.title}
                                 </div>
-                                <h3>{product.price}</h3>
+                                <h3>${product.price}</h3>
                                 <button className="btn btn-success" onClick={() => handleAdd(product)}>
                                     Add to cart
                                 </button>
